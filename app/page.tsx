37f3ba3e9b0d9bb96e6b9a707f885f0a037bf1a3
leejeff0319 <leejeff0319@gmail.com>
2023-11-03 17:48:16 -0400

    restart NEAT

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import Gameboard from '../components/Gameboard'
 
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Gameboard />
-     
-    </main>
+    <>
+      <div className="flex min-h-screen flex-col items-center justify-between p-24">
+        <Gameboard />
+      </div>
+    </>
   )
 }
